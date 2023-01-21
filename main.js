cc69a2502c87540a55a17f288e47d4526451082b
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-01-20 19:02:46 -0800

    --finish ball site

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import './style.css'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import gsap from 'gsap';
 // Scene
 
 const scene = new THREE.Scene();
@@ -11,6 +12,7 @@ const geometry = new THREE.SphereGeometry(3, 64, 64);
 
 const material = new THREE.MeshStandardMaterial({
   color: 'lightseagreen',
+  roughness: 1
 });
 
 const mesh = new THREE.Mesh(geometry, material);
@@ -27,6 +29,7 @@ const sizes = {
 
 const light = new THREE.PointLight(0xffffff, 1, 100);
 light.position.set(5, 10, 10);
+light.intensity = 1.5
 scene.add(light);
 
 // Camera
@@ -40,7 +43,7 @@ scene.add(camera);
 // Renderer
 
 const canvas = document.querySelector('.webgl');
-const renderer = new THREE.WebGLRenderer({canvas});
+const renderer = new THREE.WebGLRenderer({ canvas });
 
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(2);
@@ -59,7 +62,7 @@ window.addEventListener('resize', () => {
   console.log(window.innerWidth);
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
-  
+
   // Update Camera
   camera.aspect = sizes.width / sizes.height;
   camera.updateProjectionMatrix();
@@ -75,6 +78,25 @@ const loop = () => {
 loop()
 
 // Timeline Magic
-const tl = gspa.timeline({defaults: {duration: 1}})
+const tl = gsap.timeline({ defaults: { duration: 1 } });
 
-tl.
+tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
+
+tl.fromTo('nav', { y: "-100%" }, { y: "0%" })
+tl.fromTo(".title", { opacity: 0 }, { opacity: 1 })
+
+// Mouse Animation Color
+let mouseDown = false;
+let rgb = [];
+window.addEventListener("mousedown", () => (mouseDown = true));
+window.addEventListener("mouseup", () => (mouseDown = false));
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255), Math.round((e.pageX / sizes.height) * 255),
+      255
+    ]
+    let newColor = new THREE.Color(`rgb(${rgb.join(', ')})`)
+    gsap.to(mesh.material.color, { r: newColor.r, g: newColor.g, b: newColor.b })
+  }
+})
